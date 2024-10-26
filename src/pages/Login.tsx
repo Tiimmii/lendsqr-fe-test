0d91f282e426d0d4ec0f2911b48aef59a0f90596
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2024-10-26 14:40:55 +0100

    1. imported necessary svg's for login page 2. Wrote base tsx for login without any styling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,30 @@
 import '../components/style.scss'
+import grouped_icon from '../assets/Group.svg'
+import sign_in_picture from '../assets/pablo-sign-in 1.svg'
 
 const Login = () => {
   return (
     <div className='main-container'>
-      Login
+      <div className="logo-container">
+        <img src={grouped_icon}></img>
+      </div>
+      <div className='sign-in-picture-container'>
+        <img src={sign_in_picture}></img>
+      </div>
+      <div className="login-form">
+        <div className="login-texts-top">
+            <h1 className="welcome-primary">Welcome!</h1>
+            <span>Enter details to login.</span>
+        </div>
+        <div className="form">
+            <form>
+                <input placeholder='Email'></input>
+                <input placeholder='Password'></input>
+                <a href='#'>Forgot Password?</a>
+                <button>Login</button>
+            </form>
+        </div>
+      </div>
     </div>
   )
 }
