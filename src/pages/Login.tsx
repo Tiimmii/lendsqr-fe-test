cabe6bfdd4f6331e148b9e1388c97a9cf22115e4
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2024-10-26 16:06:02 +0100

    1. Attached Avenir-next font to src/assests/fonts and imported in css 2. Styled Login.tsx using scss

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import '../components/style.scss'
+import '../components/login.scss'
 import grouped_icon from '../assets/Group.svg'
 import sign_in_picture from '../assets/pablo-sign-in 1.svg'
 
@@ -6,23 +6,25 @@ const Login = () => {
   return (
     <div className='main-container'>
       <div className="logo-container">
-        <img src={grouped_icon}></img>
+        <img src={grouped_icon}></img>   {/* top left logo */}
       </div>
       <div className='sign-in-picture-container'>
-        <img src={sign_in_picture}></img>
+        <img src={sign_in_picture}></img> {/* Left Login picture */}
       </div>
-      <div className="login-form">
-        <div className="login-texts-top">
-            <h1 className="welcome-primary">Welcome!</h1>
-            <span>Enter details to login.</span>
-        </div>
-        <div className="form">
-            <form>
-                <input placeholder='Email'></input>
-                <input placeholder='Password'></input>
-                <a href='#'>Forgot Password?</a>
-                <button>Login</button>
-            </form>
+      <div className="login-form-container">
+        <div className="login-form">
+            <div className="login-texts-top">
+                <h1 className="welcome-primary">Welcome!</h1>
+                <span>Enter details to login.</span>
+            </div>
+            <div className="form">
+                <form>
+                    <input placeholder='Email'></input>
+                    <input placeholder='Password'></input>
+                    <a href='#'>Forgot Password?</a>
+                    <button className='login-button'>Login</button>
+                </form>
+            </div>
         </div>
       </div>
     </div>
