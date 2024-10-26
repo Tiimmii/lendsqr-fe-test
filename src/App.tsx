e00fc15905aaf631dfbbfbfd760de3cc3a0a6f8d
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2024-10-26 14:09:17 +0100

    set up routing for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import Pages from "./pages/Pages";
 
 function App() {
   return(
-    <div>Hello world!</div>
+    <BrowserRouter><Pages/></BrowserRouter>
   )
 }
 
