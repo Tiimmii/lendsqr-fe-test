3259e57bce12e7d1d2972a828b6e0940ea9d33e0
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2024-10-27 20:18:39 +0100

    Fetching records from mock api & First styling of the dashboard table
    Fetched data from mock api using useEffect, useState, and axios. Also arranged this data in a table and styled accordingly on the dashboard

---
+++ src/components/DashboardRecords.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './styles/dashboard.scss'
+
+type recordProps = {
+    records : never[]
+}
+
+const DashboardRecords = ({records}:recordProps) => {
+  return (
+    <div className="dashboard-records-container">
+        <div className="dashboard-records-table-header">
+            <div className='table-header-texts'>
+                ORGANIZATION
+                <i className="fa-solid fa-bars"></i>
+            </div>
+            <div className='table-header-texts'>
+                USERNAME
+                <i className="fa-solid fa-bars"></i>
+            </div>
+            <div className='table-header-texts'>
+                EMAIL
+                <i className="fa-solid fa-bars"></i>
+            </div>
+            <div className='table-header-texts'>
+                PHONE NUMBER
+                <i className="fa-solid fa-bars"></i>
+            </div>
+            <div className='table-header-texts'>
+                DATE JOINED
+                <i className="fa-solid fa-bars"></i>
+            </div>
+            <div className='table-header-texts'>
+                STATUS
+                <i className="fa-solid fa-bars"></i>
+            </div>
+        </div>
+        {records? records.map((item) => (
+        <div className='table-row-container'>
+            <div className="table-row" key={item["id"]}>
+                <div>{item["organization"]}</div>
+                <div>{item["username"]}</div>
+                <div>{item["email"]}</div>
+                <div>{item["phone_number"]}</div>
+                <div>{item["date_joined"]}</div>
+                <div>{item["status"]}</div>
+            </div>
+            <div className='table-options'><i className="fa-solid fa-ellipsis-vertical"></i></div>
+        </div>
+        )) : 
+        <p>No Users at this time</p>}
+    </div>
+  )
+}
+
+export default DashboardRecords
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react"
 import DashboardFloatingBoxes from "../components/DashboardFloatingBoxes"
 import DashboardHeader from "../components/DashboardHeader"
 import DashboardSideBar from "../components/DashboardSideBar"
 import '../components/styles/dashboard.scss'
+import axios from "axios"
+import DashboardRecords from "../components/DashboardRecords"
 
 const Dashboard = () => {
+    const [data, setData] = useState([])
+    useEffect(()=>{
+        axios.get('https://run.mocky.io/v3/bb22da96-cc89-4146-8c1e-b846102cbb1b')
+        .then(result=>{setData(result.data)})
+        .catch(error=>console.log(error))
+    }, [])
+    console.log(data)
   return (
     <div className="dashboard-container">
       <DashboardHeader/>
@@ -11,6 +21,7 @@ const Dashboard = () => {
       <div className="dashboard-content">
             <h1 className="dashboard-content-title">Users</h1>
             <DashboardFloatingBoxes/>
+            <DashboardRecords records={data}/>
       </div>
     </div>
   )
