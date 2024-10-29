113cd72a4fbe07fe242f06a80bc83bbfe6c8c3b3
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2024-10-29 21:16:42 +0100

    wrote and styled the user detailed page

--- src/components/UserDetailBottomBox.tsx
+++ src/components/UserDetailBottomBox.tsx
@@ -3,26 +3,101 @@ import React from 'react'
 const UserDetailBottomBox = () => {
   return (
     <div className="user-detail-bottom-box-container">
-        <div className="user-detail-bottom-box">
+        <div className="user-details">
             <p>Personal Information</p>
-            {[
-                {option_header: "FULL NAME", option_value: "Grace Effiom"},
-                {option_header: "FULL NAME", option_value: "Grace Effiom"},
-                {option_header: "FULL NAME", option_value: "Grace Effiom"},
-                {option_header: "FULL NAME", option_value: "Grace Effiom"},
-                {option_header: "FULL NAME", option_value: "Grace Effiom"},
-                {option_header: "FULL NAME", option_value: "Grace Effiom"},
-                {option_header: "FULL NAME", option_value: "Grace Effiom"},
-                {option_header: "FULL NAME", option_value: "Grace Effiom"},
-            ].map((option)=>{
-                return(
-                    <div className='outlined-user-details'>
-                        <p>{option.option_header}</p>
-                        <span>{option.option_value}</span>
-                    </div>
-                )
-            })
-            }
+            <div className="outlined-user-details-container">
+                {[
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                ].map((option)=>{
+                    return(
+                        <div className='outlined-user-details'>
+                            <p>{option.option_header}</p>
+                            <span>{option.option_value}</span>
+                        </div>
+                    )
+                })
+                }
+            </div>
+            <div className="user-detail-divider"></div>
+        </div>
+        <div className="user-details">
+            <p>Personal Information</p>
+            <div className="outlined-user-details-container">
+                {[
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                ].map((option)=>{
+                    return(
+                        <div className='outlined-user-details'>
+                            <p>{option.option_header}</p>
+                            <span>{option.option_value}</span>
+                        </div>
+                    )
+                })
+                }
+            </div>
+            <div className="user-detail-divider"></div>
+        </div>
+        <div className="user-details">
+            <p>Personal Information</p>
+            <div className="outlined-user-details-container">
+                {[
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                ].map((option)=>{
+                    return(
+                        <div className='outlined-user-details'>
+                            <p>{option.option_header}</p>
+                            <span>{option.option_value}</span>
+                        </div>
+                    )
+                })
+                }
+            </div>
+            <div className="user-detail-divider"></div>
+        </div>
+        <div className="user-details">
+            <p>Personal Information</p>
+            <div className="outlined-user-details-container">
+                {[
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                    {option_header: "FULL NAME", option_value: "Grace Effiom"},
+                ].map((option)=>{
+                    return(
+                        <div className='outlined-user-details'>
+                            <p>{option.option_header}</p>
+                            <span>{option.option_value}</span>
+                        </div>
+                    )
+                })
+                }
+            </div>
+            <div className="user-detail-divider"></div>
         </div>
     </div>
   )
